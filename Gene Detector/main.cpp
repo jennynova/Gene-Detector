67a602b0f8fac50660ea2c787f5519f38d9958bb
Jenny Nova <jennynova@jennys-macbook-pro-2.local>
2015-11-11 11:36:42 -0800

    Initial Commit

---
+++ Gene Detector/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Gene Detector
+//
+//  Created by Jenny Nova on 11/11/15.
+//  Copyright © 2015 Jenny Nova. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
